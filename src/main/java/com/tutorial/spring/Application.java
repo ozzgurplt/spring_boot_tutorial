61094c6302e0ba9eb072cb71ae47a68ee2072719
ozzgurplt <alacahan785@gmail.com>
2021-08-11 23:50:42 +0300

    Initial commit

---
+++ src/main/java/com/tutorial/spring/Application.java
@@ -0,0 +1,24 @@
+package com.tutorial.spring;
+
+import com.tutorial.spring.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+}
+
+
